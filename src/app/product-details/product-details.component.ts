16087455f1ef1f99f22e744b9db1cfb79f869e35
santyDevelop <santy_skulldevelop@hotmail.com>
2019-10-25 13:03:58 +0200

    Se ha escrito la logica de enrutado de los datos en product-details.component para obtener el producto elegido, luego se muestra ese producto especifico en la vista de product-details.html

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../products';
+
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  public product;
+
+  constructor(private route: ActivatedRoute) {
+    
+  }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(
+      params => {this.product = products[+params.get('productId')];
+    });
   }
 
 }
